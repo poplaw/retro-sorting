fd97618f18f65413c2118aa8218da1dfd82c51b9
Piotr Popławski <poplawski95@gmail.com>
2020-06-03 18:42:08 +0200

    add margin

---
+++ src/charts/utils/Margin.ts
@@ -0,0 +1,8 @@
+export default class Margin {
+    constructor(
+        readonly left: number,
+        readonly right: number,
+        readonly top: number,
+        readonly bottom: number
+    ) {}
+}
