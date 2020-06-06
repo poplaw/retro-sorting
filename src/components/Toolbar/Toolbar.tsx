942e08eeb824faa13c52c9f35dec8fc7e6d0b8a8
Piotr Popławski <poplawski95@gmail.com>
2020-06-06 16:18:38 +0200

    refactor toolbar actions to reflect recent changes

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -3,6 +3,7 @@ import { toolbar } from "./toolbar.module.scss";
 import Button from "../Button";
 import MediaControls from "../MediaControls/MediaControls";
 import DatasetDialog from "../Dialogs/Dataset";
+import AlgorithmDialog from "../Dialogs/Algorithm";
 import { useDispatch, useSelector } from "react-redux";
 import { datasetSlice, getDataset } from "../../features/dataset/datasetSlice";
 import * as d3 from "d3";
@@ -14,7 +15,8 @@ const Toolbar: FC = () => {
     const dispatch = useDispatch();
     const dataset = useSelector(getDataset);
 
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isAlgOpen, setIsAlgOpen] = useState(true);
 
     const service = useRef<SteppedSortingService>();
     if (!service.current) service.current = new SteppedSortingService(16);
@@ -22,22 +24,22 @@ const Toolbar: FC = () => {
     return (
         <nav className={toolbar}>
             <DatasetDialog isOpen={isOpen} onClose={() => setIsOpen(false)} />
-            <Button
-                variant={"is-primary"}
-                onClick={() => {
+            <AlgorithmDialog
+                isOpen={isAlgOpen}
+                onClose={() => setIsAlgOpen(false)}
+                onSelectStrategy={strategy =>
                     service.current
                         .setSortingStrategy(
-                            new InsertionSort().setSteppedSortingService(
-                                service.current
-                            )
+                            strategy.setSteppedSortingService(service.current)
                         )
                         .setOnStepDone(numbers => {
                             dispatch(
                                 datasetSlice.actions.updateDataset([...numbers])
                             );
-                        });
-                }}
-            >
+                        })
+                }
+            />
+            <Button variant={"is-primary"} onClick={() => setIsAlgOpen(true)}>
                 Algorithm
             </Button>
             <Button
