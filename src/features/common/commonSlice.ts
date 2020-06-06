1b88d18d59d2cc1b187fa67d400a469a21f47046
Piotr Popławski <poplawski95@gmail.com>
2020-06-06 18:22:08 +0200

    add info about current sorting state

--- src/features/common/commonSlice.ts
+++ src/features/common/commonSlice.ts
@@ -1,25 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "..";
 
 interface CommonState {
     name: string;
     version: string;
+    isSorting: boolean;
 }
 
 const initialState: CommonState = {
     name: "Vocabulary tester",
     version: "Alpha 0.1",
+    isSorting: false,
 };
 
 export const commonSlice = createSlice({
     name: "common",
     initialState,
-    reducers: {},
+    reducers: {
+        setIsSorting: (state, action: PayloadAction<boolean>) => {
+            state.isSorting = action.payload;
+        },
+    },
 });
 
 export const getApplicationName = (state: RootState): string =>
     state.common.name;
 
 export const getAppVerison = (state: RootState): string => state.common.version;
+
+export const isSorting = (state: RootState): boolean => state.common.isSorting;
 
 export default commonSlice.reducer;
